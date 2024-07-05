cac73f68f8a613ed03971ef7647298eac6fcccf5
Matthew <matthew_fung@hotmail.com>
2024-07-05 15:33:01 -0400

    migrate to ts

---
+++ inventory/src/App.tsx
@@ -0,0 +1,12 @@
+import builds from '@/mocks/builds.json'
+
+import React from 'react'
+
+function App() {
+  console.log('builds', builds)
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100"></div>
+  )
+}
+
+export default App
