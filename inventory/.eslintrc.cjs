993265945c8f15597c13a13cb8ee700cc9b3af69
Matthew <matthew_fung@hotmail.com>
2024-07-05 14:30:04 -0400

    fix setup

--- inventory/.eslintrc.cjs
+++ inventory/.eslintrc.cjs
@@ -10,12 +10,15 @@ module.exports = {
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'react'],
   rules: {
     'react/jsx-no-target-blank': 'off',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'react/react-in-jsx-scope': 'off', // This rule might cause issues, consider turning it off
+    'react/jsx-uses-vars': 'error',
+    'react/jsx-uses-react': 'error',
   },
 }
