1629eadad21716f02415898346c1603c0e1c2d68
Matthew <matthew_fung@hotmail.com>
2024-07-05 14:50:45 -0400

    set up shadcn

--- inventory/src/App.jsx
+++ inventory/src/App.jsx
@@ -1,11 +1,29 @@
+import { Button } from '@/components/ui/button'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
 import React from 'react'
 
 function App() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-600">
-        Hello, Vite + React + Tailwind CSS!
-      </h1>
+      <Card>
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
     </div>
   )
 }
--- inventory/vite.config.js
+++ inventory/vite.config.js
@@ -1,7 +1,17 @@
+import path from 'path'
+import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import { fileURLToPath } from 'url'
 
-// https://vitejs.dev/config/
+// because __dirname was showing undefined
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 export default defineConfig({
-  plugins: [reactRefresh()],
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './'),
+    },
+  },
 })
